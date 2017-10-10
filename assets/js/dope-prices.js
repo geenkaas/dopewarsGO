2d9f2bd84c36a58afeeacf65aaa56fb271e2d371
Geenkaas <github@geenkaas.nl>
2017-10-10 21:52:23 +0200

    Work on button handlers
    still having trouble clearing the handlers from used buttons

--- assets/js/dope-prices.js
+++ assets/js/dope-prices.js
@@ -2,56 +2,56 @@
 
 'use strict';
 
-var cashStart = 2000;
-var cashCurr = cashStart;
-function updateCash(cashNew) {
-    $('[js-cash]').find('span').html(cashNew);
-    cashCurr = parseInt($('[js-cash]').find('span').html());
-    console.log('Variable cashCurr is updated, new cashCurr = ' + cashCurr);
-};
-updateCash(cashStart);
-
-var dopelist = [];
-
-// Dope constructor function
-function Dope(name, priceMin, priceMax, amount) {
-    this.name = name
-    this.priceMin = priceMin;
-    this.priceMax = priceMax;
-    this.amount = amount;
-    this.priceCurr = function() {
-        return Math.floor(((priceMax - priceMin) + 1) * Math.random() + priceMin);
+    var cashStart = 2000;
+    var cashCurr = cashStart;
+    function updateCash(cashNew) {
+        $('[js-cash]').find('span').html(cashNew);
+        cashCurr = parseInt($('[js-cash]').find('span').html());
+        //console.log('Variable cashCurr is updated, new cashCurr = ' + cashCurr);
     };
-    //https://jsfiddle.net/Panomosh/8bpmrso1/
-    dopelist.push(this);
-};
+    updateCash(cashStart);
 
-// Dope object instance
-var dope1 = new Dope('Acid', 1000,4400, 0),
-    dope2 = new Dope('Cocaine', 15000, 29000, 1),
-    dope3 = new Dope('Hashish', 480, 1280, 0),
-    dope4 = new Dope('Heroin', 5500, 13000, 0),
-    dope5 = new Dope('Ludes', 11, 60, 9),
-    dope6 = new Dope('MDA', 1500, 4400, 0),
-    dope7 = new Dope('Opium', 540, 1250, 0),
-    dope8 = new Dope('PCP', 1000, 2500, 0),
-    dope9 = new Dope('Peyote', 220, 700, 0),
-    dope10 = new Dope('Shrooms', 630, 1300, 0),
-    dope11 = new Dope('Speed', 90, 250, 0),
-    dope12 = new Dope('Weed', 315, 890, 0);
+    var dopelist = [];
 
-//https://stackoverflow.com/questions/31180331/loop-through-each-new-object-from-constructor#31180444
-dopelist.forEach(function(dope) {
-    $('[js-dope-table-content]').append('\
-        <tr js-dope data-js-dope="'+ dope.name +'">\
-            <td><button class="button button--trade button--sell">-</button></td>\
-            <td>'+dope.name+'</td>\
-            <td js-dope-price>'+ dope.priceCurr() +'</td>\
-            <td js-dope-amount>'+ dope.amount +'</td>\
-            <td><button class="button button--trade button--buy">+</button></td>\
-        </tr>\
-    ');
-});
+    // Dope constructor function
+    function Dope(name, priceMin, priceMax, amount) {
+        this.name = name
+        this.priceMin = priceMin;
+        this.priceMax = priceMax;
+        this.amount = amount;
+        this.priceCurr = function() {
+            return Math.floor(((priceMax - priceMin) + 1) * Math.random() + priceMin);
+        };
+        //https://jsfiddle.net/Panomosh/8bpmrso1/
+        dopelist.push(this);
+    };
+
+    // Dope object instance
+    var dope1 = new Dope('Acid', 1000,4400, 0),
+        dope2 = new Dope('Cocaine', 15000, 29000, 0),
+        dope3 = new Dope('Hashish', 480, 1280, 0),
+        dope4 = new Dope('Heroin', 5500, 13000, 0),
+        dope5 = new Dope('Ludes', 11, 60, 0),
+        dope6 = new Dope('MDA', 1500, 4400, 0),
+        dope7 = new Dope('Opium', 540, 1250, 0),
+        dope8 = new Dope('PCP', 1000, 2500, 0),
+        dope9 = new Dope('Peyote', 220, 700, 0),
+        dope10 = new Dope('Shrooms', 630, 1300, 0),
+        dope11 = new Dope('Speed', 90, 250, 0),
+        dope12 = new Dope('Weed', 315, 890, 0);
+
+    //https://stackoverflow.com/questions/31180331/loop-through-each-new-object-from-constructor#31180444
+    dopelist.forEach(function(dope) {
+        $('[js-dope-table-content]').append('\
+            <tr js-dope data-js-dope="'+ dope.name +'">\
+                <td><button class="button button--trade button--sell">-</button></td>\
+                <td>'+dope.name+'</td>\
+                <td js-dope-price>'+ dope.priceCurr() +'</td>\
+                <td js-dope-amount>'+ dope.amount +'</td>\
+                <td><button class="button button--trade button--buy">+</button></td>\
+            </tr>\
+        ');
+    });
 
 $('[js-button-scootch]').on('click', function() {
     setRandomPrices();
@@ -64,54 +64,55 @@ function setRandomPrices() {
 };
 
 function updateButtons() {
+
     dopelist.forEach(function(dope) {
+        var dopeCurr = $('[data-js-dope="'+dope.name+'"]');
+        var buttonSell = dopeCurr.find('.button--sell');
+        var buttonBuy = dopeCurr.find('.button--buy');
+        var dopeCurrPrice = parseInt(dopeCurr.find('[js-dope-price]').html());
+
         if (dope.amount <= 0) {
-            $('[data-js-dope="'+dope.name+'"]').find('.button--sell').addClass('button--disabled');
+            buttonSell.addClass('button--disabled').off('click');
         } else {
-            $('[data-js-dope="'+dope.name+'"]').find('.button--sell').removeClass('button--disabled');
+            buttonSell.removeClass('button--disabled').on('click', buttonClick);
         }
-        var currDopePrice = parseInt($('[data-js-dope="'+dope.name+'"]').find('[js-dope-price]').html());
-        if (currDopePrice > cashCurr) {
-            $('[data-js-dope="'+dope.name+'"]').find('.button--buy').addClass('button--disabled');
+
+        if (dopeCurrPrice > cashCurr) {
+            buttonBuy.addClass('button--disabled').off('click');
         } else {
-            $('[data-js-dope="'+dope.name+'"]').find('.button--buy').removeClass('button--disabled');
+            buttonBuy.removeClass('button--disabled').on('click', buttonClick);
         }
+        console.log('finished updateButtons');
     });
-    buttonClick();
 };
 
 updateButtons();
 
-function buttonClick() {
-    $('.button--trade').each(function() {
-        var clickRow = $(this).closest('tr');
-        var clickDope = clickRow.attr('data-js-dope');
-        if ($(this).hasClass('button--buy')) {
-            var amount = 1;
-        } else {
-            var amount = -1;
-        };
-        $(this).not('.button--disabled').on('click', function() {
-            var cashTrade = parseInt(clickRow.find('[js-dope-price]').html());
-            var cashTemp = cashCurr - (cashTrade * amount);
-            updateCash(cashTemp);
-            updateDopeAmount(clickDope, amount);
-        });
-    });
+function buttonClick(e) {
+
+    e.preventDefault();
+
+    var clickRow = $(this).closest('tr');
+    var clickDope = clickRow.attr('data-js-dope');
+    if ($(this).hasClass('button--buy')) {
+        var amount = 1;
+    } else {
+        var amount = -1;
+    };
+    //console.log(amount);
+    var cashTrade = parseInt(clickRow.find('[js-dope-price]').html());
+    var cashTemp = cashCurr - (cashTrade * amount);
+    updateCash(cashTemp);
+    updateDopeAmount(clickDope, amount);
+    updateButtons();
 };
-
-
-$('.button--disabled').on('click', function(e) {
-    e.preventDefault;
-    e.stopPropagation;
-});
 
 function updateDopeAmount(whichDope, changeAmount) {
     //console.log(whichDope + ' ' + changeAmount);
-    for (var s in dopelist) {
-        if (dopelist[s].name == whichDope) {
-            dopelist[s].amount += changeAmount;
-            $('[data-js-dope="'+ whichDope +'"]').find('[js-dope-amount]').html(dopelist[s].amount);
+    for (var dope in dopelist) {
+        if (dopelist[dope].name == whichDope) {
+            dopelist[dope].amount += changeAmount;
+            $('[data-js-dope="'+ whichDope +'"]').find('[js-dope-amount]').html(dopelist[dope].amount);
         }
     }
 };
