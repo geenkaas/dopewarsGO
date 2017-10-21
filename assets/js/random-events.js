00a3f0e8282dca42038af7817796db6b35268199
Geenkaas <github@geenkaas.nl>
2017-10-21 11:19:12 +0200

    FIX for random events
    THX Michael!

--- assets/js/random-events.js
+++ assets/js/random-events.js
@@ -1,56 +1,75 @@
-// random-events.js
-// Scripts for handling random events.
-
-'use scrict'
-
 // Create event list
 var eventList = [];
 var eventChance = 0;
 var eventRNG = Math.random();
 
 // Dope constructor function
-function event(name, chance) {
+function Event(name, chance) {
     this.name = name
     this.chance = chance;
     //https://jsfiddle.net/Panomosh/8bpmrso1/
     eventList.push(this);
 }
-
 // event object instance
-var event1 = new event('oldLady', 5),
-    event2 = new event('officerHardass', 10),
-    event3 = new event('dopeRaid', 5),
-    event4 = new event('dopeSupply', 5),
-    event5 = new event('dopeFound', 20),
-    event6 = new event('findCoat', 5);
+var event1 = new Event('oldLady', 10),
+    event2 = new Event('officerHardass', 20),
+    event3 = new Event('dopeRaid', 5),
+    event4 = new Event('dopeSupply', 20),
+    event5 = new Event('dopeFound', 30),
+    event6 = new Event('findCoat', 5),
+    event7 = new Event('pennyFound', 20),
+    event7 = new Event('marrakeshExpress', 10);
 
+function getEventChange(Event, total) {
+// Total value could be calculated within function or own function.
+//  console.log(Event);
+//  console.log("getEventChange: " + Event.chance*(100/total));
+    return Event.chance * (100 / total);
+};
+
+function getRandomEvent(eventMap) {
+    console.log(eventRNG * 100);
+    for (var i = 0; i < eventMap.length; i++) {
+        //console.log(eventMap[i].chance >= eventRNG*100);
+        if (eventMap[i].chance >= eventRNG * 100) {
+            console.log("returning " + eventMap[i].name);
+            eventRNG = Math.random();
+            return eventMap[i];
+        }
+    }
+};
+
+var eventTotal = 0;
+var eventMap = [];
 //https://stackoverflow.com/questions/31180331/loop-through-each-new-object-from-constructor#31180444
 eventList.forEach(function(event) {
     //console.log(event.name);
-    eventChance += event.chance;
-    
-    console.log(eventList);
-    eventList.addedChance = 'eventChance';
+    //eventChance = event.chance;
+    //console.log(eventList);
+    //eventList.addedChance = 'eventChance';
     //console.log(eventChance);
-})
+    eventTotal += event.chance;
+    //console.log(event.name + ': eventtotal: ' + eventTotal);
+
+});
+
+var range = 0;
+eventList.forEach(function(event) {
+    range += getEventChange(event, eventTotal);
+    eventMap.push(new Event(event.name, range ));
+    //console.log(event.name + " chance: " + getEventChange(event, eventTotal) + '%. Range <= ' + range);
+});
+//console.log(eventMap);
 
 function randomEvents() {
     var randomEventChance = Math.random();
-    var randomChanceTrigger = 0.9;
-    if (randomEventChance > 1 - randomChanceTrigger) {
-        //console.log('Fire Random event!' + randomEventChance);
+    var randomChanceTrigger = 0.5;
+    if (randomEventChance >= 1 - randomChanceTrigger) {
         randomChanceFire();
     }
 };
 
 function randomChanceFire() {
-
-    var eventAllChances = (eventOldLady + eventHardass + eventDopeRaid + eventDopeSupply + eventDopeFound);
-    //console.log(eventAllChances);
-    var eventSingleChance = Math.random();
-    var eventChanceTimeAll = eventSingleChance * eventAllChances;
-
-    if (eventChanceTimeAll < 1) {
-
-    }
+    var randomEvent = getRandomEvent(eventMap);
+    //console.log(randomEvent);
 };
