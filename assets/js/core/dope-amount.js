4025f0d9e7fb20c3e73a3d37bd5a6bf88d6cb2cb
Geenkaas <github@geenkaas.nl>
2017-10-24 22:04:18 +0200

    ARNE | REMOVE console logs

--- assets/js/core/dope-amount.js
+++ assets/js/core/dope-amount.js
@@ -5,7 +5,7 @@
 function updateDopeAmount(whichDope, changeAmount) {
     // console.log(whichDope + ' ' + changeAmount);
     for (var dope in dopelist) {
-        console.log(dope);
+        //console.log(dope);
         if (dopelist[dope].name == whichDope) {
             dopelist[dope].amount += changeAmount;
             $('[data-js-dope="'+ whichDope +'"]').find('[js-dope-amount]').html(dopelist[dope].amount);
