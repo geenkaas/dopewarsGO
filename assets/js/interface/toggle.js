e4e930f2ee3e2328b0611d44af74b7833fa46f5c
Geenkaas <github@geenkaas.nl>
2017-10-28 15:22:10 +0200

    ARNE | Work

--- assets/js/interface/toggle.js
+++ assets/js/interface/toggle.js
@@ -1,7 +1,9 @@
-//mainmenu.js
+// mainmenu.js
 
 'use strict';
 
 $('[js-toggle]').on('click', function() {
-	var target = $(this).attr('data-target');
+    var target = $(this).attr('data-target');
+    //console.log(target);
+    $('.' + target).toggleClass('active').slideToggle();
 })
