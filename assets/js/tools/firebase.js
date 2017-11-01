acc96c961cb4e8c798199611f809950d9ad4909d
Arne Maier <github@geenkaas.nl>
2017-11-01 16:04:58 +0100

    ARNE | ADD firebase script

---
+++ assets/js/tools/firebase.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+
+'use strict';
+
+var config = {
+    apiKey: "AIzaSyD3KptP-Zcb7wDW_f1QjXfd5vzKyPhiovs",
+    authDomain: "dopewars-go.firebaseapp.com",
+    databaseURL: "https://dopewars-go.firebaseio.com",
+    projectId: "dopewars-go",
+    storageBucket: "dopewars-go.appspot.com",
+    messagingSenderId: "532369668543"
+};
+firebase.initializeApp(config);
+
+
+// TODO create a email/Facebook/Twitter/Github login
+// https://github.com/firebase/quickstart-js/tree/master/auth
