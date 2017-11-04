c61b4435106ca47ef9d5b060bab92afe17c3e0d2
Geenkaas <github@geenkaas.nl>
2017-11-04 11:48:47 +0100

    ARNE | FIX for name popup, now in always on mode...

--- assets/js/popups/player-name.js
+++ assets/js/popups/player-name.js
@@ -5,33 +5,39 @@
 function popupPlayerName() {
     //console.log(readCookie('playerName'));
 
+        clearCookie('playerName');
+
     if (readCookie('playerName') === 'null') {
         $('[js-popup-playerName]').addClass('active').show();
         playerNameSelect();
-    } else {
     }
 
 }
 
 function playerNameSelect() {
-    var initials = $('[js-tag]').find('.c-playerInitials').hide();
+    //$('[js-tag]').find('.c-playerInitial').hide();
+    var initials = $('[js-tag]').find('.c-playerInitial');
     initials.first().addClass('active');
     $('.c-playerLetter').on('click', function() {
         var whichLetter = $(this).html();
-        console.log(whichLetter);
-        var next = $('[js-tag]').find('.c-playerInitials.active').html(whichLetter).removeClass('active').next();
+        //console.log(initials)
+        
+        //console.log(whichLetter);
+        var next = $('.c-playerInitial.active').removeClass('active').html(whichLetter).next();
+        //console.log(next.length);
         if (next.length) {
+            //console.log('next!');
             next.addClass('active');
         }
         else {
-            initials.addClass('active');
+            //console.log('no more!!!');
+            initials.removeClass('active');
             $('[js-button-saveName]').removeClass('button--disabled').on('click', function() {
                 var newName = '';
                 initials.each(function() {
                     newName += $(this).html();
                 })
                 createCookie('playerName', newName);
-                console.log(newName);
                 console.log(readCookie('playerName'));
                 $('[js-popup-playerName]').fadeOut(200);
             });
