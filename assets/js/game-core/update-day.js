18c619b166a67a13b2636870b75508371095fb0a
Geenkaas <github@geenkaas.nl>
2017-11-06 19:58:43 +0100

    ARNE | ADD new start for choice at end of game

--- assets/js/game-core/update-day.js
+++ assets/js/game-core/update-day.js
@@ -5,33 +5,45 @@
 function updateDay() {
     setRandomPrices();
     if (player.dayCurr >= player.dayMax) {
-
-        dopelist.forEach(function(dope) {
-            var dopeCurr = $('[data-js-dope="'+ dope.name +'"]');
-            var dopeAmount = parseInt(dopeCurr.find('[js-dope-amount]').html());
-            if (dopeAmount > 0) {
-
-                var cashLeftOver = parseInt(dopeCurr.find('[js-dope-price]').html());
-                player.cash += cashLeftOver * dopeAmount;
-            }
-        })
-        updateStats();
         removeSlide($(this).closest('.c-slide'));
 
-        var endScore = digits(player.cash)
 
         var eventContent = ('\
             <h2>Doperun over!</h2>\
-            <p>What a run! Your final score is '+ endScore +'</p>\
+            <p>What a run! You still have some dope in your pockets, what do you want to do with them?</p>\
             <div class="c-button-group">\
-                <div class="button" js-game-restart>Again!</div>\
+                <div class="button" js-game-end-sell>Sell them</div>\
+                <div class="button" js-game-end-send>Send to my house</div>\
             </div>\
         ');
+        //<p>What a run! Your final score is '+ endScore +'</p>\
         createModal(eventContent);
+
+        $('[js-game-end-sell]').on('tap', function() {
+            sellRemainingDope()
+        })
+
+
+        var endScore = digits(player.cash)
+        sellRemainingDope();
+
         restartButtons();
     } else {
         player.dayCurr += 1;
         updateStats();
         randomEvents();
     }
+}
+
+function sellRemainingDope() {
+    dopelist.forEach(function(dope) {
+        var dopeCurr = $('[data-js-dope="'+ dope.name +'"]');
+        var dopeAmount = parseInt(dopeCurr.find('[js-dope-amount]').html());
+        if (dopeAmount > 0) {
+
+            var cashLeftOver = parseInt(dopeCurr.find('[js-dope-price]').html());
+            player.cash += cashLeftOver * dopeAmount;
+        }
+    })
+    updateStats();
 }
