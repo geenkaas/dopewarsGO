c5dfba34cee1f2ccb118981398ba561094d5cb3f
Geenkaas <github@geenkaas.nl>
2017-11-13 21:18:38 +0100

    ARNE | ADD increment counter for gamesplayed

--- assets/js/firebase/firestore.js
+++ assets/js/firebase/firestore.js
@@ -4,24 +4,52 @@
 
 function initFirestore() {
 
-    var fs = firebase.firestore();
-    var fb = firebase.auth();
-    var userId = fb.currentUser.uid;
-
-    var arcadeName = readCookie('playerName');
-
-    var userData = fs.doc('players/'+ userId);
-
     var buttonGameNew = $('[js-game-start]');
+    var buttonGameEnd = $('[js-game-end]');
+
+    var buttonGameWon = $('[js-game-end]');
+    var buttonGameLost = $('[js-game-end]');
+
+    //var gamesPlayed = fb.currentUser.gamesPlayed;
+    //console.log(gamesPlayed);
 
     buttonGameNew.on('tap', function() {
-        userData.set({
-            uid: userId,
-            arcadeName: arcadeName
+
+        var fb = firebase.auth();
+        var userId = fb.currentUser.uid;
+
+        var arcadeName = readCookie('playerName');
+
+        var db = firebase.firestore();
+        // Below are same results
+        var userData = db.collection('players').doc(userId);
+        // But I prefer this one
+        var userData = db.doc('players/'+ userId);
+
+        var gamesPlayedNew;
+
+        userData.get().then(function(doc) {
+        if (doc.exists) {
+            //console.log("Document data:", doc.data());
+
             // increment data by one
             // See: https://firebase.google.com/docs/database/web/read-and-write#save_data_as_transactions
-            // gameStarted: +=1 
-        })
-    })
+            gamesPlayedNew = doc.data().gamesPlayed + 1;
 
+            userData.update({
+                uid: userId,
+                arcadeName: arcadeName,
+                gamesPlayed: gamesPlayedNew
+            });
+        } else {
+            console.log("No such document!");
+        }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+    })
+}
+
+window.onload = function() {
+    initFirestore();
 }
