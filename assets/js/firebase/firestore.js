ea22310b05cf95ad61055cb227eb90aaac963133
Geenkaas <github@geenkaas.nl>
2017-11-16 19:33:11 +0100

    ARNE |  Fix test and update gamesPlayed

--- assets/js/firebase/firestore.js
+++ assets/js/firebase/firestore.js
@@ -22,43 +22,50 @@ function updateScore(property, change) {
     // New modular function for button handling.
     // Optimal: call function like: updateScore(killedByHardass, 1); and add one to your deaths deu to hardass.
 
-        var fb = firebase.auth();
-        var userId = fb.currentUser.uid;
 
-        var arcadeName = readCookie('playerName');
+    var fb = firebase.auth();
+    var userId = fb.currentUser.uid;
 
-        var fs = firebase.firestore();
-        // Below are same results
-        var userData = fs.collection('players').doc(userId);
-        // But I prefer this one
-        var userData = fs.doc('players/'+ userId);
+    var arcadeName = readCookie('playerName');
 
-        var propertyNew;
+    var fs = firebase.firestore();
+    // Below are same results
+    var userData = fs.collection('players').doc(userId);
+    // But I prefer this one
+    var userData = fs.doc('players/'+ userId);
 
-        userData.get().then(function(doc) {
-        if (doc.exists) {
-            //console.log("Document data:", doc.data());
+    var propertyNew;
 
-            // increment data by one
-            // See: https://firebase.google.com/docs/database/web/read-and-write#save_data_as_transactions
-            if (doc.data()[property] < 1) {
-                propertyNew = 1;
-            } else {
-                propertyNew = doc.data()[property] + change;
-            }
+    userData.update({
+        uid: userId,
+        arcadeName: arcadeName
+    });
 
-            userData.update({
-                uid: userId,
-                arcadeName: arcadeName,
-                // This does not work!
-                property: propertyNew
-            });
+    userData.get().then(function(doc) {
+    if (doc.exists) {
+        //console.log("Document data:", doc.data());
+
+        // increment data by one
+        // See: https://firebase.google.com/docs/database/web/read-and-write#save_data_as_transactions
+        console.log(doc.data()[property]);
+        if (typeof doc.data()[property] === 'undefined') {
+            console.log('property not found');
+            propertyNew = 1;
         } else {
-            console.log("No such document!");
+            console.log('added '+ change +' to '+ property);
+            propertyNew = doc.data()[property] + change;
         }
-        }).catch(function(error) {
-            console.log("Error getting document:", error);
-        });
+
+        var updatedObj = {};
+        updatedObj[property] = propertyNew;
+        userData.update(updatedObj);
+
+    } else {
+        console.log("No such document!");
+    }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
 }
 
 window.onload = function() {
