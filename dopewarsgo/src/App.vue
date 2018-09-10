e505c26f22d306b18143470e67221a1a3937e552
Arne Maier <github@geenkaas.nl>
2018-09-10 15:21:42 +0200

    ARNE | CONNECT style scss files and remove old tutorial

--- dopewarsgo/src/App.vue
+++ dopewarsgo/src/App.vue
@@ -51,13 +51,7 @@ export default {
 };
 </script>
 
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "scss/style.scss";
 </style>
--- dopewarsgo/src/Dopelist.vue
+++ dopewarsgo/src/Dopelist.vue
@@ -53,5 +53,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
 </style>
